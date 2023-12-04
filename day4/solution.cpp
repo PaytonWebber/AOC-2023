c5a000a71086d5ed6915911d0b0fff96484e481d
Payton Webber <patyonwebber@gmail.com>
2023-12-03 22:37:25 -0800

    day4 part2 solution

--- day4/solution.cpp
+++ day4/solution.cpp
@@ -55,7 +55,6 @@ Card create_card(string line)
 
 int get_card_points(Card card)
 {
-
     for (int i = 0; i < card.my_numbers.size(); i++)
     {
         for (int j = 0; j < card.winning_numbers.size(); j++)
@@ -71,19 +70,65 @@ int get_card_points(Card card)
     return pow(2, card.matches - 1);
 }
 
+int get_card_copies(int main_card, vector<Card> cards)
+{
+    clog << "\rMain card: " << main_card << " " << flush;
+
+    if (cards[main_card].matches == 0)
+    {
+        for (int i = 0; i < cards[main_card].my_numbers.size(); i++)
+        {
+            for (int j = 0; j < cards[main_card].winning_numbers.size(); j++)
+            {
+                if (cards[main_card].my_numbers[i] == cards[main_card].winning_numbers[j])
+                {
+                    cards[main_card].matches++;
+                }
+            }
+        }
+    }
+    int copies = 1;
+    for (int i = 1; i <= cards[main_card].matches && (main_card + i) < cards.size(); i++)
+    {
+        copies += get_card_copies(main_card + i, cards);
+    }
+
+    return copies;
+}
+
 void parse_input()
 {
     int output = 0;
     int count = 0;
     Card card;
 
+    #ifdef PART2
+    vector<Card> cards;
+    #endif
+
     string line;
     while (getline(cin, line))
     {
         count++;
         card = create_card(line);
+
+        #ifdef PART2
+        cards.push_back(card);
+        #endif
+
+        #ifdef PART1
         output += get_card_points(card);
+        #endif
+
     }
+
+    #ifdef PART2
+    for (int i = 0; i < cards.size(); i++)
+    {
+        output += get_card_copies(i, cards);
+    }
+    #endif
+
     cout << output << endl;
 }
 
